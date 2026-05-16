81ab9f50926eee5f24b8aa5490029941ee81a453
Youssef <ya103857@gmail.com>
2026-05-16 23:53:55 +0300

    Add files via upload

---
+++ Function(2).py
@@ -0,0 +1,27 @@
+# function : return
+def dis_play():
+ 	return "Youssef"
+name = dis_play()
+print(name)
+print("*"*10)
+
+def add (x , y):
+	   return x*y
+	
+result = add(13,20)
+print ("result is ",result)
+
+add(12,8)
+print(add(12,8))
+print("*"*15)
+
+def add(x,y):
+	return x+y
+def dis_play(x,y):
+	return x+y
+	
+result1 = add(10,20)
+result2 = dis_play(15,14)
+
+print ("Result return1 ",result1)
+print ("Result return2 ",result2)
